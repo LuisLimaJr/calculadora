033b46e04b55d08a657ad493be49234fe15e786a
Luis Lima <luis.limajr@hotmail.com>
2022-05-15 07:47:05 -0300

    First commit

---
+++ R/somar.R
@@ -0,0 +1,14 @@
+#'Função para somar
+#'
+#'Esta função serve para somar dois numeros
+#'
+#'@param a um numero
+#'@param b outro numero
+#'
+#'@examples
+#'somar (3,4)
+#'@export
+somar <- function(a,b){
+  a+b
+}
+
---
+++ R/subtrair.R
@@ -0,0 +1,16 @@
+#'Função para subtrair
+#'
+#'Esta função serve para subtrair dois numeros
+#'
+#'@param a um numero
+#'@param b outro numero
+#'
+#'@examples
+#'subtrair (3,4)
+#'@export
+
+
+
+subtrair <- function(a,b){
+  a-b
+}
